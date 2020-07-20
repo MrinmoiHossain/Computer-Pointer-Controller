573f0bc6a3b3342b71c1ae321d6325cefe7539cf
MrinmoiHossain <mokit.aust@gmail.com>
2020-07-20 17:47:50 +0600

    preprocess output function implement

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,4 +1,5 @@
 from model import Model
+import cv2
 
 class FaceDetectionModel(Model):
     def __init__(self, model, device = 'CPU', extensions = None):
@@ -6,15 +7,30 @@ class FaceDetectionModel(Model):
 
         self.load_model(model, device, extensions)
 
-    def predict(self, image):
-        image = self.preprocess_input(image)
+    def predict(self, image, threshold = 0.6):
         self.exec_net(image)
 
         if self.wait() == 0:
             outputs = self.exec_network.requests[0].outputs[self.output_blob]
-            detections, crop_image = self.preprocess_output(outputs, image)
+            image, detections = self.preprocess_output(outputs, image, threshold)
         
-    def preprocess_output(self, outputs, image):
+    def preprocess_output(self, outputs, image, threshold = 0.6):
         width = int(image.shape[1])
         height = int(image.shape[0])
-        ## TODO
+        
+        faces = []
+
+        for i in range(len(outputs[0][0])):
+            box = outputs[0][0][i]
+            confidence = box[2]
+            if confidence >= threshold:
+                xmin = int(box[3] * width)
+                ymin = int(box[4] * height)
+                xmax = int(box[5] * width)
+                ymax = int(box[6] * height)
+
+                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255,0,0), 1)
+                faces.append([xmin, ymin,xmax, ymax])
+
+        return image, faces
+
